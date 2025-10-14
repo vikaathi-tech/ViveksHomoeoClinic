4b8748c952faeb24f26f5c93460a3a86d9b153e7
Gokul BJ <gokul.bj@outlook.com>
2025-10-15 00:30:17 +0530

    Frontend addition

---
+++ Frontend/src/assets/brand/sygnet.js
@@ -0,0 +1,15 @@
+export const sygnet = [
+  '100 64',
+  `<g fill="currentColor" fill-rule="evenodd">
+    <text
+      x="0"
+      y="48"
+      font-family="Poppins, Arial, sans-serif"
+      font-size="35"
+      font-weight="800"
+      letter-spacing="4"
+    >
+      VHC
+    </text>
+  </g>`,
+];
